d170b5f40b3db6605319f051c6da97c467884804
John Dent <denty@moutai.local>
2018-03-22 20:43:07 +0000

    Version 0.3.

--- pg_fdw_mv_rewrite--0.2.2--0.3.sql
+++ pg_fdw_mv_rewrite--0.2.2--0.3.sql
@@ -3,12 +3,12 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_fdw_mv_rewrite" to load this file. \quit
 
-CREATE FUNCTION pg_fdw_mv_rewrite_handler()
+CREATE or replace FUNCTION pg_fdw_mv_rewrite_handler()
 RETURNS fdw_handler
 AS 'MODULE_PATHNAME.0.3'
 LANGUAGE C STRICT;
 
-CREATE FUNCTION pg_fdw_mv_rewrite_validator(text[], oid)
+CREATE or replace FUNCTION pg_fdw_mv_rewrite_validator(text[], oid)
 RETURNS void
 AS 'MODULE_PATHNAME.0.3'
 LANGUAGE C STRICT;
