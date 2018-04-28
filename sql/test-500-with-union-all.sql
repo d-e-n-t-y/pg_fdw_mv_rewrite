a74192a18794cdb45a51661248094cba0f887d1b
John Dent <denty@qqdd.eu>
2018-04-28 11:35:08 +0100

    Strange this didn't go with previous commit...

---
+++ sql/test-500-with-union-all.sql
@@ -0,0 +1,3 @@
+-- alter server loopback options (add log_match_progress 'true');
+
+explain verbose select key, case when count(value) < 10 then 'several' when count(value) < 100 then 'tens' when count(value) < 1000 then 'hundreds' when count(value) < 10000 then 'thousands' when count(value) < 100000 then 'tens of thousands' when count(value) < 1000000 then 'hundreds of thousands' when count(value) < 10000000 then 'millions' else 'many' end as quantity FROM test_lb GROUP BY key union all select key, count(value)::text FROM test_lb GROUP BY key;
