832c74d902a396d520ba31d32a547e8833b717a9
John Dent <denty@qqdd.eu>
2018-07-08 09:34:44 +0100

    More compensation for unstable plans in tests.

--- sql/test-701-select-with-distinct.sql
+++ sql/test-701-select-with-distinct.sql
@@ -1,11 +1,14 @@
 -- Anticipate message "query requires unsupported features"
+-- We don't actually care about the plan itself, only the log output.
 set mv_rewrite.log_match_progress = 'true';
-
-set mv_rewrite.log_match_progress = 'true';
-explain (VERBOSE, COSTS OFF)
-select distinct key, hidden, count (value)
-  from test
-  group by key, hidden;
+set mv_rewrite.rewrite_enabled_for_tables = '';
+-- Anticipate no rewrite
+DO LANGUAGE plpgsql $$ DECLARE t text; BEGIN FOR t IN
+	explain (VERBOSE, COSTS OFF)
+		select distinct key, hidden, count (value)
+  		from test
+  		group by key, hidden
+LOOP IF t LIKE '%Rewritten%' THEN RAISE 'Rewritten'; END IF; END LOOP; END; $$;
 
 select distinct key, hidden, count (value)
   from test
