a055a1ec1515f5c34313fbf6c08c6ac2b4e6ef18
John Dent <denty@qqdd.eu>
2019-05-31 22:15:36 +0100

    Modified test to account for variation in output 11 vs. 12beta1.

--- sql/test-802-complex-ts-test.sql
+++ sql/test-802-complex-ts-test.sql
@@ -1,6 +1,6 @@
 explain (verbose, costs off)
-select timestamp, value
+select timestamp, to_char (value, '999999D9999') as value
 	from ordered_ts ('2018-06-09 00:00:00+01', '2018-06-10 00:00:00+01', 'Room 1', 'TEMPERATURE', 'N/A', 'minute', 15);
 
-select timestamp, value
+select timestamp, to_char (value, '999999D9999') as value
 from ordered_ts ('2018-06-09 00:00:00+01', '2018-06-10 00:00:00+01', 'Room 1', 'TEMPERATURE', 'N/A', 'minute', 15);
